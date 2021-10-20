e0fecce3b838c36283508c0671c67e119f1c9c84
xhayper <hayper1919@gmail.com>
2021-10-20 23:30:18 +0700

    Better type annotation

--- src/Base/Event.ts
+++ src/Base/Event.ts
@@ -1,9 +1,11 @@
 import BetterClient from "./BetterClient";
 import * as Discord from "discord.js";
 
+type EventFunction = (client: Discord.Client | BetterClient, ...args: any[]) => void
+
 export type Event = {
     eventName: keyof Discord.ClientEvents,
-    on?: (client: Discord.Client | BetterClient, ...args: any[]) => void,
-    off?: (client: Discord.Client | BetterClient, ...args: any[]) => void,
-    once?: (client: Discord.Client | BetterClient, ...args: any[]) => void
+    on?: EventFunction,
+    off?: EventFunction,
+    once?: EventFunction
 }
