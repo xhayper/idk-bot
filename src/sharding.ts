47413224e37d15d70066442f32198166c4eb796b
xhayper <hayper1919@gmail.com>
2021-10-20 23:38:30 +0700

    mmm, path.join stuff...

--- src/sharding.ts
+++ src/sharding.ts
@@ -2,11 +2,11 @@ import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 import * as path from "path";
 
-dotenv.config({ path: path.join(__dirname, "../") + "\\.env" });
+dotenv.config({ path: path.join(__dirname, "../.env")});
 
 if (!process.env.DISCORD_TOKEN) process.exit(-1);
 
-const manager = new Discord.ShardingManager(path.join(__dirname, "./Index.js"), { token: process.env.DISCORD_TOKEN });
+const manager = new Discord.ShardingManager(path.join(__dirname, "./index.js"), { token: process.env.DISCORD_TOKEN });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
