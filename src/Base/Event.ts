083d16a9902df34a6274c8b91ad1c570dbe120c5
xhayper <hayper1919@gmail.com>
2021-10-21 09:16:21 +0700

    Change from EventFunction to EventListener

--- src/Base/Event.ts
+++ src/Base/Event.ts
@@ -1,11 +1,11 @@
 import BetterClient from "./BetterClient";
 import * as Discord from "discord.js";
 
-type EventFunction = (client: Discord.Client | BetterClient, ...args: any[]) => void
+type EventListener = (client: Discord.Client | BetterClient, ...args: any[]) => void
 
 export type Event = {
     eventName: keyof Discord.ClientEvents,
-    on?: EventFunction,
-    off?: EventFunction,
-    once?: EventFunction
+    on?: EventListener,
+    off?: EventListener,
+    once?: EventListener
 }
